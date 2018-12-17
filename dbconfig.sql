c63641beffaa2810e8aec412d629f25a993fe0c0
Michael Smith <msmithboise@gmail.com>
2018-12-16 22:22:45 -0700

    SQL table added

---
+++ dbconfig.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE contacts
+-- (
+--     contact_id INT,
+--     name VARCHAR(20),
+--     number INT,
+--     email VARCHAR(20),
+--     address VARCHAR (50),
+--     PRIMARY KEY (contact_id)
+-- );
+
+DESCRIBE contacts;
