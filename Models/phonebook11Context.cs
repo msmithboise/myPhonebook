8ec6a7e4097b4ff9490fc4f99a2973c541d30c5e
Michael Smith <msmithboise@gmail.com>
2018-12-16 22:07:02 -0700

    new repo with entity

---
+++ Models/phonebook11Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace myPhonebook.Models
+{
+    public partial class phonebook11Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=den1.mysql5.gear.host;database=port=3306;database=phonebook11;user id=phonebook11;password=An6I?7zu-j31");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
